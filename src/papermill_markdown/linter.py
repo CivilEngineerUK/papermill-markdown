bca6edc758259371e3faccf886d256e0de589222
CivilEngineerUK <michael.rustell25@gmail.com>
2025-02-02 20:51:22 +0000

    minor change to linter again

--- src/papermill_markdown/linter.py
+++ src/papermill_markdown/linter.py
@@ -19,14 +19,15 @@ class MarkdownLinter:
     # Optionally, a similar pattern can be defined for figure captions if desired.
     FIGURE_CAPTION_PATTERN = re.compile(r'^(Figure|Fig|FIGURE|FIG)\.?\s*\d+', re.IGNORECASE)
 
-    def __init__(self):
+    def __init__(self, markdown_text: str):
         """
         Initialize the linter with the Markdown text.
         """
+        self.original_text = markdown_text
         self.fixed_text = None
         self.issues = []  # List of tuples: (line number, description)
 
-    def lint(self, markdown_text) -> (list, str):
+    def lint(self) -> (list, str):
         """
         Lint and auto-format the Markdown.
 
@@ -36,7 +37,7 @@ class MarkdownLinter:
             - The fixed Markdown text as a single string
         """
         # First, use mdformat to standardize the Markdown.
-        formatted_text = mdformat.text(markdown_text)
+        formatted_text = mdformat.text(self.original_text)
         lines = formatted_text.splitlines()
 
         fixed_lines = []
