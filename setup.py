52dc4693c8f018e5e2f996b65b801d213fd39c4a
CivilEngineerUK <michael.rustell25@gmail.com>
2025-02-02 20:13:58 +0000

    minor changes to setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 # setup.py
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-  name="papermill-markdown",
-  version="0.1.0",
-  description="A converter to transform Markdown into Papermill JSON payloads",
-  author="Michael Rustell",
-  author_email="mike@inframatic.ai",
-  url="https://github.com/CivilEngineerUK/papermill-markdown",
-  packages=find_packages(where="src"),
-  package_dir={"": "src"},
-  install_requires=[
-      "pydantic",
-      "requests",
-      "python-dotenv"
-  ],
-  classifiers=[
-      "Programming Language :: Python :: 3",
-      "License :: OSI Approved :: MIT License",
-      "Operating System :: OS Independent",
-  ],
-  python_requires='>=3.6',
+    name="papermill-markdown",
+    version="0.1.1",
+    packages=["papermill_markdown"],
+    description="A converter to transform Markdown into Papermill JSON payloads",
+    author="Michael Rustell",
+    author_email="mike@inframatic.ai",
+    url="https://github.com/CivilEngineerUK/papermill-markdown",
+    package_dir={"": "src"},
+    install_requires=[
+        "pydantic",
+        "requests",
+        "python-dotenv"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
