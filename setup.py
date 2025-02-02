ca0eeb4dc77c78142344480175e11de5dac28f51
CivilEngineerUK <michael.rustell25@gmail.com>
2025-02-02 23:18:37 +0000

    version 0.1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="papermill-markdown",
-    version="0.1.5",
+    version="0.1.6",
     packages=["papermill_markdown"],
     description="A converter to transform Markdown into Papermill JSON payloads",
     author="Michael Rustell",
